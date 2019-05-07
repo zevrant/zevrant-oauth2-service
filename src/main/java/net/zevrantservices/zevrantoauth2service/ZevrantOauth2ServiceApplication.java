f4783c31538ccd3fbf18a66d1c6976bcd548e9d1
EC2 Default User <ec2-user@ip-172-31-33-74.ec2.internal>
2019-05-07 20:37:55 +0000

    initial commit

---
+++ src/main/java/net/zevrantservices/zevrantoauth2service/ZevrantOauth2ServiceApplication.java
@@ -0,0 +1,13 @@
+package net.zevrantservices.zevrantoauth2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZevrantOauth2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZevrantOauth2ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/zevrantservices/zevrantoauth2service/ZevrantOauth2ServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package net.zevrantservices.zevrantoauth2service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZevrantOauth2ServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
