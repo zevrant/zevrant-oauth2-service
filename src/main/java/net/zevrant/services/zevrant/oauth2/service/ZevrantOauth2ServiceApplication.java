7a7c5c4b2ee0fc015845e3f6ae2e5b5f3577453a
gdittrick <gerethd@gmail.com>
2020-03-24 16:38:26 -0400

    updates eureka url

--- src/main/java/net/zevrant/services/zevrant/oauth2/service/ZevrantOauth2ServiceApplication.java
+++ src/main/java/net/zevrant/services/zevrant/oauth2/service/ZevrantOauth2ServiceApplication.java
@@ -3,8 +3,11 @@ package net.zevrant.services.zevrant.oauth2.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ZevrantOauth2ServiceApplication {
 
