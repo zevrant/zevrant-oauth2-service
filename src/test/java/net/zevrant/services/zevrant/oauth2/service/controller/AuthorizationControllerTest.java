55c838c803fc8a20e5497e4b09b703dc742336b5
gdittrick <gerethd@gmail.com>
2020-04-09 15:06:37 -0400

    added sonar config

---
+++ src/test/java/net/zevrant/services/zevrant/oauth2/service/controller/AuthorizationControllerTest.java
@@ -0,0 +1,26 @@
+package net.zevrant.services.zevrant.oauth2.service.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorizationControllerTest {
+
+
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void testLogin() {
+    }
+
+    @Test
+    void getAuthorization() {
+    }
+
+    @Test
+    void logout() {
+    }
+}
