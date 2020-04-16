b86f2ee2adcb40909006d5d71c8564f6dda4882c
gdittrick <gerethd@gmail.com>
2020-04-16 18:04:10 -0400

    commented out integration tests

--- src/integrationTest/java/net/zevrant/services/zevrant/oauth2/service/controller/AuthorizationControllerTest.java
+++ src/integrationTest/java/net/zevrant/services/zevrant/oauth2/service/controller/AuthorizationControllerTest.java
@@ -1,26 +1,23 @@
 package net.zevrant.services.zevrant.oauth2.service.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AuthorizationControllerTest {
-
-
-
-    @Test
-    void login() {
-    }
-
-    @Test
-    void testLogin() {
-    }
-
-    @Test
-    void getAuthorization() {
-    }
-
-    @Test
-    void logout() {
-    }
-}
+//@SpringBootTest
+//class AuthorizationControllerTest {
+//
+//
+//
+//    @Test
+//    void login() {
+//    }
+//
+//    @Test
+//    void testLogin() {
+//    }
+//
+//    @Test
+//    void getAuthorization() {
+//    }
+//
+//    @Test
+//    void logout() {
+//    }
+//}
