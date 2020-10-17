9d03c93c460113081441403dc57c5e08291bec53
zevrant <gerethd@gmail.com>
2020-10-17 15:25:29 -0400

    FT_ZHS-17 updated get username by token repository call to get a list of tokens

--- src/main/java/net/zevrant/services/zevrant/oauth2/service/users/UserProvider.java
+++ src/main/java/net/zevrant/services/zevrant/oauth2/service/users/UserProvider.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.ClientRegistrationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Primary
@@ -43,10 +44,10 @@ public class UserProvider implements ClientDetailsService {
     }
 
     public ClientDetails loadClientByToken(String token) {
-        Optional<Token> tokenDb = tokenRepository.findByToken(token);
+        List<Token> tokenDb = tokenRepository.findAllByToken(token);
         if (tokenDb.isEmpty()) {
             return null;
         }
-        return loadClientByClientId(tokenDb.get().getClientId());
+        return loadClientByClientId(tokenDb.get(0).getClientId());
     }
 }
