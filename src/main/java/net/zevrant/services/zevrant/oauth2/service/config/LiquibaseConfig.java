837784830e34d6ea63b49d2dd7bbc922f47d199b
Zevrant <gerethd@gmail.com>
2021-01-28 12:01:05 -0500

    FT_ZHS-20 fixed lock killer

--- src/main/java/net/zevrant/services/zevrant/oauth2/service/config/LiquibaseConfig.java
+++ src/main/java/net/zevrant/services/zevrant/oauth2/service/config/LiquibaseConfig.java
@@ -3,23 +3,44 @@ package net.zevrant.services.zevrant.oauth2.service.config;
 import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.Arrays;
 
 import static java.lang.String.format;
 
+@Configuration
 public class LiquibaseConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(LiquibaseConfig.class);
 
+    private final ConfigurableApplicationContext applicationContext;
+
+    @Autowired
+    public LiquibaseConfig(ConfigurableApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @Bean
-    public SpringLiquibase liquibase(SpringLiquibase liquibase, DataSource dataSource) {
+    public SpringLiquibase liquibase(DataSource dataSource) {
         removeDBLock(dataSource);
+        SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
+        String[] profiles = applicationContext.getEnvironment().getActiveProfiles();
+        if(Arrays.asList(profiles).contains("develop")) {
+            liquibase.setChangeLog("classpath:db/changelog/db.changelog-master-develop.yaml");
+        } else if(Arrays.asList(profiles).contains("local")) {
+            liquibase.setChangeLog("classpath:db/changelog/db.changelog-master-local.yaml");
+        } else {
+            liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+        }
         return liquibase;
     }
 
@@ -27,9 +48,11 @@ public class LiquibaseConfig {
 
     private void removeDBLock(DataSource dataSource) {
 
-        //Timestamp, currently set to 5 mins or older.
+        //Timestamp, currently set to 3 mins or older.
 
         final Timestamp lastDBLockTime = new Timestamp(System.currentTimeMillis() - (3 * 60 * 1000));
+
+        logger.error(lastDBLockTime.toString());
 
         final String query = format("DELETE FROM DATABASECHANGELOGLOCK WHERE LOCKED=true AND LOCKGRANTED<'%s'", lastDBLockTime.toString());
 
